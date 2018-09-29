b4798c65db7802f95e47a6efcd0fba046d5f05d1
a.kalugin <japanknight2007@gmail.com>
2018-09-29 14:09:41 +0300

    Добавил вывод данных пользователя функцией show

--- resources/views/admin/userShow.blade.php
+++ resources/views/admin/userShow.blade.php
@@ -1,5 +1,39 @@
 @extends('adminlte::page')
 
 @section('content')
-<strong>{{ $user->id }}</strong>
+
+    <div class="container">
+<table class="table table-striped table-bordered box">
+
+    <tbody>
+    <tr>
+        <td>ID</td>
+        <td>{{ $user->id }}</td>
+    </tr>
+    <tr>
+        <td>Name</td>
+        <td>{{ $user->name }}</td>
+    </tr>
+    <tr>
+        <td>Email</td>
+        <td>{{ $user->email  }}</td>
+    </tr>
+    <tr>
+        <td>Phone</td>
+        <td>{{ $user->phone  }}</td>
+    </tr>
+    <tr>
+        <td>Created_at</td>
+        <td>{{ $user->created_at  }}</td>
+    </tr>
+    <tr>
+        <td>Updated_at</td>
+        <td>{{ $user->updated_at  }}</td>
+    </tr>
+    </tbody>
+    <div class="box-footer">
+        <a type="button" class="btn btn-default" href="{{ url('/admin/user/index') }}">Назад</a>
+    </div>
+</table>
+    </div>
 @endsection
